d1cd8491bc47d882cd3adee728bbfa58dffb051a
Victor Hugo <victorferrarihugo@hotmail.com>
2025-11-17 19:26:41 -0300

    fix: Create function to return a single Engine value (Singleton Pattern)

--- app/database/create_db.py
+++ app/database/create_db.py
@@ -1,24 +1,42 @@
 from sqlalchemy import create_engine
+from sqlalchemy.engine.base import Engine
 import traceback
 import os
 from dotenv import load_dotenv
-from models import Base
+from app.database.models import Base
 
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
+_ENGINE: Engine | None = None
 
 
-def create_database_schema():
-    print("Conectando ao banco de dados...")
+def get_engine() -> Engine | None:
+    global _ENGINE
 
+    if _ENGINE is None:
+        print("Criando nova Engine...")
+        try:
+            _ENGINE = create_engine(
+                url=DATABASE_URL,
+                echo=True
+            )
+            print("✅ Nova Engine criada com sucesso!")
+        
+        except Exception as e:
+            print("Houve um erro inesperado ao criar a Engine!", e)
+            traceback.print_exc()
+            _ENGINE = None
+            raise 
+
+    return _ENGINE
+
+
+def create_database_schema(engine: Engine):
+    """Cria o esquema do banco de dados (todas as tabelas) se não existirem."""
+
+    print("Criando tabelas...")
     try:
-        engine = create_engine(
-            url=DATABASE_URL,
-            echo=True
-        )
-
-        print("✅ Conexão realizada com sucesso!")
         print("Criando tabelas...")
 
         Base.metadata.create_all(engine)
@@ -30,5 +48,12 @@ def create_database_schema():
         traceback.print_exc()
 
 
+
 if __name__ == "__main__":
-    create_database_schema()
+    try:
+        engine_instance = get_engine()
+        create_database_schema(engine=engine_instance)
+
+    except Exception as e:
+        print("Falha ao cria banco de dados!", e)
+        traceback.print_exc()
