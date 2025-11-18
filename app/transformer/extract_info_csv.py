f4f7b3d1b8d09b3e5023f3591114970234caaa23
Victor Hugo <victorferrarihugo@hotmail.com>
2025-11-17 21:24:50 -0300

    feat: Remove load_data function to load_data file

--- app/transformer/extract_info_csv.py
+++ app/transformer/extract_info_csv.py
@@ -1,11 +1,8 @@
 import pandas as pd
 from typing import List
-from app.database.create_db import get_engine
-from app.database.models import Cnae
-from sqlalchemy.orm import Session
+
 
 SEPARATOR = ";"
-ENGINE = get_engine()
 
 def read_csv_chuncked(file_path: str, chunk_size: int = 10000, columns_name: List[str] = None):
     """Lê um arquivo CSV em partes (chunks) para processamento em lotes."""
@@ -18,34 +15,3 @@ def read_csv_chuncked(file_path: str, chunk_size: int = 10000, columns_name: Lis
         names=columns_name,
         low_memory=False
     )
-
-
-def load_cnaes(file_path, engine):
-    column_names = ["codigo_cnae", "descricao"]
-    total_loaded = 0
-
-    try:
-        chunks = read_csv_chuncked(file_path=file_path, columns_name=column_names)
-        
-        for chunk in chunks:
-            chunk = chunk[["codigo_cnae", "descricao"]].copy() 
-
-            chunk["codigo_cnae"] = chunk["codigo_cnae"].astype(str)
-            chunk["descricao"] = chunk["descricao"].astype(str)
-
-            with Session(engine) as session:
-                chunk.to_sql(Cnae.__tablename__, engine, if_exists="append", index=False)
-                session.commit()
-            
-            total_loaded += len(chunk)
-        
-        print(f"Carga de CNAEs concluída. Total de registros: {total_loaded}")
-
-    except Exception as e:
-        print("Falha ao salvar dados do Cnaes no banco de dados.", e)
-        print("Abortando...")
-
-
-if __name__ == "__main__":
-    # Exemplo...
-    load_cnaes(file_path="dados_cnpj/2025-11/Cnaes.csv", engine=ENGINE)
